d988b798898a9f66edf0148c8c9f2558c5c2237c
mezhuang <306637137@qq.com>
2018-06-11 01:56:56 +0800

    新增推荐人

--- yuanlian.sql
+++ yuanlian.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2018-06-10 10:06:24
+Date: 2018-06-11 01:56:12
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -73,16 +73,18 @@ CREATE TABLE `group_info` (
   `group_name` varchar(50) DEFAULT NULL COMMENT '分组角色名称',
   `group_code` varchar(5) DEFAULT NULL COMMENT '用户类型：01系统管理员,02分销商,03客户,04导购员,05分销经理',
   PRIMARY KEY (`group_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of group_info
 -- ----------------------------
 INSERT INTO `group_info` VALUES ('18', '系统管理员', '10001');
 INSERT INTO `group_info` VALUES ('19', '家居顾问', '10002');
-INSERT INTO `group_info` VALUES ('20', '分销经理', '10003');
-INSERT INTO `group_info` VALUES ('21', '分销经纪人', '10004');
-INSERT INTO `group_info` VALUES ('22', '普通用户', '10005');
+INSERT INTO `group_info` VALUES ('20', '分销经纪人', '10003');
+INSERT INTO `group_info` VALUES ('21', '分销经理', '10004');
+INSERT INTO `group_info` VALUES ('22', '分销总监', '10005');
+INSERT INTO `group_info` VALUES ('23', '推荐客户', '10006');
+INSERT INTO `group_info` VALUES ('24', '普通客户', '10007');
 
 -- ----------------------------
 -- Table structure for `group_user_map`
@@ -93,7 +95,7 @@ CREATE TABLE `group_user_map` (
   `group_id` varchar(50) DEFAULT NULL,
   `user_id` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`group_user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of group_user_map
@@ -104,8 +106,9 @@ INSERT INTO `group_user_map` VALUES ('20', '19', '63');
 INSERT INTO `group_user_map` VALUES ('21', '19', '64');
 INSERT INTO `group_user_map` VALUES ('22', '18', '18');
 INSERT INTO `group_user_map` VALUES ('23', '19', '18');
-INSERT INTO `group_user_map` VALUES ('24', '20', '18');
-INSERT INTO `group_user_map` VALUES ('25', '21', '18');
+INSERT INTO `group_user_map` VALUES ('35', '21', '18');
+INSERT INTO `group_user_map` VALUES ('42', '20', '18');
+INSERT INTO `group_user_map` VALUES ('44', null, '116');
 
 -- ----------------------------
 -- Table structure for `power_group_map`
@@ -228,30 +231,28 @@ CREATE TABLE `test` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info` (
-  `user_id` varchar(50) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `open_id` varchar(30) NOT NULL COMMENT '微信唯一标识',
+  `user_dear_name` varchar(30) DEFAULT NULL,
   `user_name` varchar(50) DEFAULT NULL COMMENT '用户姓名',
-  `user_phone` varchar(12) DEFAULT NULL COMMENT '用户电话',
+  `user_phone` varchar(20) DEFAULT NULL COMMENT '用户电话',
   `user_reg_time` varchar(24) DEFAULT NULL COMMENT '用户注册时间',
-  `user_type` varchar(3) DEFAULT NULL,
-  `employee_code` varchar(20) DEFAULT NULL,
-  `superior_code` varchar(30) DEFAULT NULL COMMENT '上级',
+  `manager_phone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   KEY `user_id` (`user_id`),
   KEY `user_id_2` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=117 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_info
 -- ----------------------------
-INSERT INTO `user_info` VALUES ('100', 'ozLOG5LWXLjDpxZLrD1DxXIKx888', '善哥', '155241223', '2018-06-03 01:18:55', null, null, null);
-INSERT INTO `user_info` VALUES ('105', 'fffff', null, null, '2018-06-03 11:34:07', null, null, null);
-INSERT INTO `user_info` VALUES ('106', 'fffff', null, null, '2018-06-03 11:37:48', null, null, null);
-INSERT INTO `user_info` VALUES ('107', 'fffff', 'yyy', '15264', '2018-06-03 11:42:29', null, null, null);
-INSERT INTO `user_info` VALUES ('108', 'fffff', 'yyy', '15264', '2018-06-03 11:46:17', null, null, null);
-INSERT INTO `user_info` VALUES ('113', 'fffff', 'yyy', '15264', '2018-06-03 11:56:08', null, null, null);
-INSERT INTO `user_info` VALUES ('114', 'fffff', 'xiaoliu', '18812345678', '2018-06-03 12:36:55', null, null, null);
-INSERT INTO `user_info` VALUES ('18', 'ozLOG5LWXLjDpxZLrD1DxXIKxHWg', '袁仕旭', '15208945149', null, null, null, '08001');
-INSERT INTO `user_info` VALUES ('61', '', '分销经理', '18876175897', null, null, '08001', null);
-INSERT INTO `user_info` VALUES ('62', '', '周芬', '15208985488', null, null, null, null);
-INSERT INTO `user_info` VALUES ('63', '', '小白', '15201245203', null, null, null, null);
+INSERT INTO `user_info` VALUES ('63', '', null, '小白', '15201245203', null, null);
+INSERT INTO `user_info` VALUES ('100', 'ozLOG5LWXLjDpxZLrD1DxXIKx888', null, '善哥', '155241223', '2018-06-03 01:18:55', null);
+INSERT INTO `user_info` VALUES ('105', 'fffff', null, null, null, '2018-06-03 11:34:07', null);
+INSERT INTO `user_info` VALUES ('106', 'fffff', null, null, null, '2018-06-03 11:37:48', null);
+INSERT INTO `user_info` VALUES ('107', 'fffff', null, 'yyy', '15264', '2018-06-03 11:42:29', null);
+INSERT INTO `user_info` VALUES ('108', 'fffff', null, 'yyy', '15264', '2018-06-03 11:46:17', null);
+INSERT INTO `user_info` VALUES ('113', 'fffff', null, 'yyy', '15264', '2018-06-03 11:56:08', null);
+INSERT INTO `user_info` VALUES ('114', 'fffff', null, 'xiaoliu', '18812345678', '2018-06-03 12:36:55', null);
+INSERT INTO `user_info` VALUES ('115', '222', null, 'mm', '158', null, null);
+INSERT INTO `user_info` VALUES ('116', 'ozLOG5LWXLjDpxZLrD1DxXIKxHWg', null, 'xd', '123', '2018-06-11 01:55:31', '232');
